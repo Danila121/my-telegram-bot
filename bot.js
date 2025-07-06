91a5de2ef3e035ab3d4227f220326001782a1bb6
Danila121 <kabanovdanila006@gmail.com>
2025-07-06 17:50:52 +1000

    Добавлено несоклько комманд бота

--- bot.js
+++ bot.js
@@ -4,10 +4,36 @@ const TelegramBot = require('node-telegram-bot-api');
 function startBot(){
   const token = process.env.TELEGRAM_BOT_TOKEN;
   const bot = new TelegramBot(token, { polling: true });
-  bot.onText(/\/start/, (msg) => {
+  //Команды бота
+  bot.onText(/\/start/, (msg)=>{
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Привет, октагон!');
   });
+  
+  bot.onText(/\/help/, (msg)=>{
+    const chatId = msg.chat.id;
+    const lines = [
+      "Доступные команды:",
+      "/help - список команд",
+      "/site - ссылка на сайт Октагона",
+      "/creator - информация о создателе",
+      "/start - приветствие бота"
+    ];
+    const text = lines.join('\n');
+    bot.sendMessage(chatId, text);
+  });
+
+  bot.onText(/\/site/, (msg=>{
+    const chatId = msg.chat.id;
+    const octagonUrl = 'https://octagon-students.ru/';
+    bot.sendMessage(chatId, octagonUrl);
+  }))
+
+  bot.onText(/\/creator/, (msg=>{
+    const chatId = msg.chat.id;
+    const creatorInfo = 'Danila Kabanov';
+    bot.sendMessage(chatId, creatorInfo);
+  }))
 }
 
 module.exports = startBot;
